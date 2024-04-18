734d02524ae3f974c4fffcf53201f7c0074349b0
HrsFlex <harsh42026@gmail.com>
2024-04-18 23:23:15 +0530

    Scripting done

--- script.js
+++ script.js
@@ -6,17 +6,82 @@ const wpm = document.querySelector('.wap span');
 const cpm = document.querySelector('.cpm span');
 const btn = document.querySelector('button');
 
+//set values
+let timer;
+let maxTime = 60;
+let timeLeft = maxTime;
+let charIndex = 0;
+let mistake = 0;
+let isTyping = false;
+
 function loadParagraph(){
-    const paragraph = [" lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident sunt in culpa qui officia deserunt mollit anim id est laborum."];
+    const paragraph = ["Use a low flow shower head, aerators, or a cistern that measures how much water is flushed"];
     
     const randomIndex = Math.floor(Math.random() * paragraph.length);
-    typingText.innerHtml='';
-    for(const char of paragraph(randomIndex)){
+    typingText.innerHTML='';
+    for(const char of paragraph[randomIndex]){
         console.log(char);
-        typingText.innerHtml+= `<span>
-        ${char} </span>` ;
+        typingText.innerHTML+= `<span>${char}</span>` ;
 
     }
     typingText.querySelectorAll('span')[0].classList.add('active');
+    document.addEventListener('keydown',()=>{input.focus()});
 }
+
+//Handle user input
+
+function initTyping(){
+    const char = typingText.querySelector('span');
+    const typedChar= input.value.charAt(charIndex);
+    if(charIndex < char.length && timeLeft >0){
+
+        if(!isTyping){
+            timer = setInterval(initTime,1000);
+            isTyping = true;
+        }
+
+        if(char[charIndex].innerText === typedChar){
+            char[charIndex].classList.add('correct');
+        }else{
+            mistake ++;
+        char[charIndex].classList.add('incorrect');
+        }
+    charIndex++;
+    char[charIndex].classList.add('active');
+    mistakes.innerText = mistake;
+    cpm.innerText = charIndex-mistake;
+    }else{
+        clearInterval(timer);
+        input.value='';
+    }
+}
+
+function initTime(){
+    if(timeLeft > 0){
+        timeLeft--;
+        time.innerText = timeLeft;
+        let wpmVal = Math.round(((charIndex - mistake)/5 )/(maxTime - timeLeft)*60);
+        wpm.innerText = wpmVal;
+    }
+else{
+    clearInterval(timer);
+    }
+}
+
+function reset(){
+    loadParagraph();
+    clearInterval(timer);
+    timeLeft = maxTime;
+    time.innerText = timeLeft;
+    input.value='';
+    charIndex = 0;
+    mistake=0;
+    isTyping=false;
+    wpm.innerText=0;
+    cpm.innerText=0;
+    mistakes.innerText=0;
+}
+
+input.addEventListener("input", initTyping);
+btn.addEventListener("click", reset);
 loadParagraph();
